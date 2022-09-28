9c9c88b77d0dc5f0ca2245703010ceec4dbc5c77
Reinaldo Abdo <reinaldoabdo@hotmail.com>
2022-09-28 15:40:03 -0400

    Login e logos

--- src/components/CmpLogin.vue
+++ src/components/CmpLogin.vue
@@ -1,23 +1,26 @@
 <template>
   <q-page
-    class="bg-grey-10 window-height window-width row justify-center items-center"
+    class="window-height window-width row justify-center items-center"
+    style="background-color: #2c2c2c"
   >
     <div class="column">
+      <q-card
+        rounded-borders
+        bordered
+        bg-yellow-8
+        class="q-pa-lg q-mb-sm shadow-1"
+      >
+        <img alt="Cempre logo" src="~assets/logo-cempre-sem-fundo.png" />
+      </q-card>
       <div class="row">
-        <img
-          alt="Quasar logo"
-          src="~assets/logo-cempre-sem-fundo.svg"
-          style="width: 360px"
-        />
-      </div>
-      <div class="row">
-        <q-card square bordered class="q-pa-lg shadow-1">
+        <q-card rounded-borders bordered class="q-pa-lg shadow-1">
           <q-card-section>
             <q-form class="q-gutter-md">
               <q-input
                 square
                 filled
                 clearable
+                outlined
                 v-model="email"
                 type="email"
                 label="email"
@@ -26,6 +29,7 @@
                 square
                 filled
                 clearable
+                outlined
                 v-model="password"
                 type="password"
                 label="password"
@@ -34,11 +38,11 @@
           </q-card-section>
           <q-card-actions class="q-px-md">
             <q-btn
-              unelevated
-              color="light-green-7"
+              color="yellow-8"
+              text-color="grey-8"
               size="lg"
               class="full-width"
-              label="Login"
+              label="Entrar"
             />
           </q-card-actions>
           <q-card-section class="text-center q-pa-none">
